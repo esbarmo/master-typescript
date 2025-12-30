2d7fe8eb669eebdff6017e804633c3ab4251f989
esbarmo <estelabarcelo_34@hotmail.com>
2025-12-30 19:29:01 +0100

    Modulos

---
+++ javascript-moderno/scripts/13-modulos.js
@@ -0,0 +1,7 @@
+import imprimirArray, {saludo} from "./14-mi-modulo.js";
+
+console.log("Archivo main de mi proyecto");
+document.body.style = "background:pink"
+
+let nombres = ["Dani", "Leo", "Estela"];
+saludo("Hola");
---
+++ javascript-moderno/scripts/14-mi-modulo.js
@@ -0,0 +1,20 @@
+export default function imprimirArray(datos) {
+    datos.forEach(element => {
+        console.log(element);
+    });
+
+    return datos;
+}
+
+export function saludo(nombre){
+    console.log(nombre);
+    
+    return nombre;
+}
+
+/*
+export default {
+    imprimirArray,
+    saludo
+};
+*/
